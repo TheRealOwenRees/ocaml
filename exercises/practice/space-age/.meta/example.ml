03121c3d22f95875b690299d6fd02df14578c834
Owen Rees <owenrees.dev@gmail.com>
2026-03-02 12:17:51 +0100

    update return type to Result.t, use strings instead of options

--- exercises/practice/space-age/.meta/example.ml
+++ exercises/practice/space-age/.meta/example.ml
@@ -1,22 +1,19 @@
 open Base
 
-type planet = Mercury | Venus | Earth | Mars
-            | Jupiter | Saturn | Neptune | Uranus
-
 let earth_years seconds = seconds /. 31557600.0
-
 let rel_years = function
-    | Mercury -> 0.2408467
-    | Venus   -> 0.61519726
-    | Earth   -> 1.
-    | Mars    -> 1.8808158
-    | Jupiter -> 11.862615
-    | Saturn  -> 29.447498
-    | Uranus  -> 84.016846
-    | Neptune -> 164.79132
+    | "Mercury" -> 0.2408467
+    | "Venus"   -> 0.61519726
+    | "Mars"    -> 1.8808158
+    | "Earth"   -> 1.
+    | "Jupiter" -> 11.862615
+    | "Saturn"  -> 29.447498
+    | "Uranus"  -> 84.016846
+    | "Neptune" -> 164.79132
+    | _         -> 1.
 
 let age_on planet seconds =
     let seconds' = Float.of_int seconds in
     match planet with
-    | Earth -> earth_years seconds' 
-    | _     -> earth_years seconds' /. rel_years planet
+    | "Mercury" | "Venus" | "Earth" | "Mars" | "Jupiter" | "Saturn" | "Uranus" | "Neptune" -> Ok (earth_years seconds' /. rel_years planet)
+    | _ -> Error "not a planet"
--- exercises/practice/space-age/space_age.ml
+++ exercises/practice/space-age/space_age.ml
@@ -1,5 +1,2 @@
-type planet = Mercury | Venus | Earth | Mars
-            | Jupiter | Saturn | Neptune | Uranus
-
 let age_on _ _ =
     failwith "'age_on' is missing"
